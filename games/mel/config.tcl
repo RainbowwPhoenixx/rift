e6d71509a06068602014d704d32face6d3773476
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2021-03-30 08:31:14 +0200

    Added mel splits, renamed portal2 to match sr.c.

---
+++ games/mel/config.tcl
@@ -0,0 +1,38 @@
+title "Portal Stories: Mel" "Story Mode (Inbounds)"
+timer
+splits {
+	"1952" {
+		"Tram Ride" .
+		"Mel Intro" .
+		"Lift" .
+		"Garden" .
+	}
+	"Extended Relaxation" {
+		"Destroyed Garden" .
+		"Underbounce" .
+		"Once Upon" .
+		"Past Power" .
+		"Ramp" .
+		"Firestorm" .
+	}
+	"The Ascent" {
+		"Junkyard" .
+		"Concepts" .
+		"Paint Fling" .
+		"Faith Plate" .
+		"Transition" .
+	}
+	"Organic Complications" {
+		"Overgrown" .
+		"Funnel Over Goo" .
+		"Two of a Kind" .
+		"Destroyed" .
+		"Factory" .
+	}
+	"Intrusion" {
+		"Core Access" .
+		"Finale" .
+	}
+}
+
+splitter "| ./splitters/sar/sar_split"
